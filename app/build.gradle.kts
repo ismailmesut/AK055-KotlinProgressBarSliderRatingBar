a39a918bef8021a30457b4f9aada6d9be77d56ed
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-20 18:30:15 +0300

    updated: progress bar, seek bar, rating bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinprogressbarsliderratingbar"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinprogressbarsliderratingbar"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinprogressbarsliderratingbar/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinprogressbarsliderratingbar/MainActivity.kt
@@ -1,13 +1,53 @@
 package com.ismailmesutmujde.kotlinprogressbarsliderratingbar
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinprogressbarsliderratingbar.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.buttonStart.setOnClickListener {
+            bindingMain.progressBar.visibility = View.VISIBLE
+        }
+
+        bindingMain.buttonStop.setOnClickListener {
+            bindingMain.progressBar.visibility = View.INVISIBLE
+        }
+
+        bindingMain.slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                bindingMain.textViewResult.text = "Result : $progress"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        bindingMain.buttonShow.setOnClickListener {
+            val incomingProgress = bindingMain.slider.progress
+            val incomingRating = bindingMain.ratingBar.rating
+
+            Log.e("Progress", incomingProgress.toString())
+            Log.e("Rating", incomingRating.toString())
+        }
 
     }
 }
